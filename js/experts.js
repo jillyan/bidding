d4dd2de4c76a23860d9cbe57f031c8811e59c7c4
jillyan <zyan@talend.com>
2018-03-22 22:24:16 +0800

    basic

---
+++ js/experts.js
@@ -0,0 +1,33 @@
+function change_panel(id,id_hide){
+	$('#panel'+id).show();
+	$('#panel'+id_hide).hide();
+	$('#nav'+id).toggleClass("active");
+	$('#nav'+id_hide).toggleClass("active");
+}
+
+Array.prototype.contains = function ( needle ) {
+  for (i in this) {
+    if (this[i] == needle) return true;
+  }
+  return false;
+}
+var names = new Array();
+function toggle_expert(name){
+	var idx =$.inArray(name, names);
+	console.log(name + idx);
+	if( idx != -1){
+		console.log(name + 'in array');
+		names.pop(name);
+	}else{		
+		names.push(name);
+	}
+	var content = "";
+	$.each(names, function (index, value) {  
+		content += "<li>"+names[index]+"</li>";        
+   });
+	$("#total").empty();
+	$("#total").append(names.length);
+
+	$("#expert_list").empty();
+	$("#expert_list").append(content);  
+}
