e4127a7d81dae4cde379dc01117e126c8a8870b7
jillyan <zyan@talend.com>
2018-03-27 18:00:14 +0800

    设置打印时实际样式

--- js/experts.js
+++ js/experts.js
@@ -1,33 +1,43 @@
-function change_panel(id,id_hide){
-	$('#panel'+id).show();
-	$('#panel'+id_hide).hide();
-	$('#nav'+id).toggleClass("active");
-	$('#nav'+id_hide).toggleClass("active");
+function change_panel(id, id_hide) {
+    $('#panel' + id).show();
+    $('#panel' + id_hide).hide();
+    $('#nav' + id).toggleClass("active");
+    $('#nav' + id_hide).toggleClass("active");
 }
 
-Array.prototype.contains = function ( needle ) {
-  for (i in this) {
-    if (this[i] == needle) return true;
-  }
-  return false;
+Array.prototype.contains = function (needle) {
+    for (i in this) {
+        if (this[i] == needle) return true;
+    }
+    return false;
 }
 var names = new Array();
-function toggle_expert(name){
-	var idx =$.inArray(name, names);
-	console.log(name + idx);
-	if( idx != -1){
-		console.log(name + 'in array');
-		names.pop(name);
-	}else{		
-		names.push(name);
-	}
-	var content = "";
-	$.each(names, function (index, value) {  
-		content += "<li>"+names[index]+"</li>";        
-   });
-	$("#total").empty();
-	$("#total").append(names.length);
 
-	$("#expert_list").empty();
-	$("#expert_list").append(content);  
+function toggle_expert(name) {
+    var idx = $.inArray(name, names);
+    console.log(name + idx);
+    if (idx != -1) {
+        console.log(name + 'in array');
+        names.pop(name);
+    } else {
+        names.push(name);
+    }
+    var content = "";
+    $.each(names, function (index, value) {
+        content += "<li>" + names[index] + "</li>";
+    });
+    $("#total").empty();
+    $("#total").append(names.length);
+
+    $("#expert_list").empty();
+    $("#expert_list").append(content);
+}
+
+function doPrint() {
+    $('#print_id').print({
+        globalStyles: true,
+        mediaPrint: false,
+        stylesheet:'./css/print.css',
+        title:'',
+    });
 }
